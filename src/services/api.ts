d31fcc66ed63f984c72be30cfcb147f07871b8da
thiagoassisk8 <tyagosilva82@gmail.com>
2024-05-12 21:54:03 -0300

    api connecting to mock

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const api = axios.create()
 
 export async function getStoreDetails() {
   const { data } = await api.get(
-    'https://98d36ac94d36499f89398905f4d4f609.api.mockbin.io/',
+    'http://localhost:3000/products',
   )
 
   return data
